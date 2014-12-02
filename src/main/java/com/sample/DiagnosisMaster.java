afd4a07c68743c29fb6a652d84507c2d784f3a2b
Dominik Lochynski <dominiklochynski@gmail.com>
2014-12-02 12:51:19 +0100

    Committing in a submodule

--- src/main/java/com/sample/DiagnosisMaster.java
+++ src/main/java/com/sample/DiagnosisMaster.java
@@ -1,6 +1,34 @@
 package com.sample;
 
 public class DiagnosisMaster extends Thread {
+	private String sex;
+	private boolean overEighteeen; 
+	private boolean chestPain = false;
+	private boolean stuffiness = false;
+	private boolean steppedUpBreath = false;
 	
-
+	public DiagnosisMaster(String sex, boolean overEighteeen) {
+		super();
+		this.sex = sex;
+		this.overEighteeen = overEighteeen;
+	}
+	public boolean isSteppedUpBreath() {
+		return steppedUpBreath;
+	}
+	public void setSteppedUpBreath(boolean steppedUpBreath) {
+		this.steppedUpBreath = steppedUpBreath;
+	}
+	public boolean isChestPain() {
+		return chestPain;
+	}
+	public void setChestPain(boolean chestPain) {
+		this.chestPain = chestPain;
+	}
+	public boolean isStuffiness() {
+		return stuffiness;
+	}
+	public void setStuffiness(boolean stuffiness) {
+		this.stuffiness = stuffiness;
+	}
+	
 }
