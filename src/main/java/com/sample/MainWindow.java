3eea28e112d20d346e4a8eb4a9d121fa472b0e99
Karol Scipniak <karol.scipniak@gmail.com>
2014-12-02 13:46:10 +0100

    gui practical ready now

--- src/main/java/com/sample/MainWindow.java
+++ src/main/java/com/sample/MainWindow.java
@@ -15,12 +15,14 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.UIManager;
 
 public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
 	public JPanel questionPanel;
 	public JPanel imagePanel;
+	public JButton startButton;
 
 //	/**
 //	 * Launch the application.
@@ -51,6 +53,7 @@ public class MainWindow extends JFrame {
 		contentPane.setLayout(null);
 		
 		imagePanel = new JPanel();
+		imagePanel.setBackground(Color.WHITE);
 		imagePanel.setBounds(531, 84, 393, 477);
 		contentPane.add(imagePanel);
 		imagePanel.setLayout(new BorderLayout(0, 0));
@@ -68,13 +71,26 @@ public class MainWindow extends JFrame {
 		
 		questionPanel = new JPanel();
 		questionPanel.setBackground(Color.WHITE);
-		questionPanel.setBounds(10, 84, 511, 393);
+		questionPanel.setBounds(10, 157, 511, 320);
 		contentPane.add(questionPanel);
 		questionPanel.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.WHITE);
 		panel_3.setBounds(10, 488, 511, 73);
 		contentPane.add(panel_3);
 		panel_3.setLayout(null);
+		
+		startButton = new JButton("Start new diagnosis!");
+		startButton.setFont(new Font("SansSerif", Font.PLAIN, 17));
+		startButton.setBackground(new Color(51, 153, 255));
+		startButton.setForeground(Color.WHITE);
+		startButton.setBounds(148, 11, 212, 51);
+		panel_3.add(startButton);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(10, 84, 511, 62);
+		contentPane.add(panel);
 	}
 }
